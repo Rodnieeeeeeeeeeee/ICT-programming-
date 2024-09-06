050e3ae6a8aee2c1a88480625058ad4fa5942ae6
Rodnieeeeeeeeeeee <rodnieespeso075@gmail.com>
2024-09-06 18:33:47 +0800

    Add files via upload

---
+++ SwitchDemo.java
@@ -0,0 +1,14 @@
+public class Swtich {
+	public static void main(String[] args) {
+		 
+		 byte dayOfWeek = 1;
+		 
+		 switch(dayOfweek) {
+		     default:
+		      System.out.println("Monday");
+		      break;
+		      
+		 }
+		 
+	}
+}
