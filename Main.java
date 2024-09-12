a3e8bb781d396f00b786974575894b52347e00da
Rodnieeeeeeeeeeee <rodnieespeso075@gmail.com>
2024-09-12 11:55:42 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main{
+   public class void main(String[] args){
+   //instantiate ... creating a copy of
+   Animal.animal = new Animal();
+   
+   
+   //call the method named setName;
+   //reference.method
+   animal.setName("glenn");
+   animal.setId(696969);
+   
+   
+   System.out.println(animal.getName());
+   System.out.println(animal.getId());
+   System.out.println(animal.getAge());
+
+
+
+   
+   
+   }
